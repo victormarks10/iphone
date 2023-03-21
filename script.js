f3ef2b20107f6aad9428e0710b1ab1440e52e6c1
Victormarks10 <aaron.victormarks10@gmail.com>
2023-03-21 20:38:21 -0300

    Adicionando Projeto

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function trocaCor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaImagem(imagem){
+    let imgIphone = document.querySelector(".iphone")
+    imgIphone.src = imagem
+}
+
+
+
